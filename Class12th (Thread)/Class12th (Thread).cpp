b3271eee21af33cd036b43f7ca45031275dfd339
JeongAaron <lovejsk1004@gmail.com>
2025-02-06 19:41:09 +0900

    2025-02-06 벡터

---
+++ Class12th (Thread)/Class12th (Thread).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void input()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "input..." << endl;
+	}
+}
+
+void Renderer()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Renderer" << endl;
+	}
+}
+
+int main()
+{
+#pragma region 스레드
+	//	프로세스 내에서 실행되는 하나의 작업 단위
+	//	thread thread1(input);
+	//	thread thread2(Renderer);
+	//	thread1.join();
+	//	thread2.join();
+#pragma endregion
+}
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-#pragma region 스레드
-	//	프로세스 내에서 실행되는 하나의 작업 단위
-
+#pragma region vector container
+	vector<int> vector;
+	for (int i = 0; i < 5; i++)
+	{
+		vector.push_back((i + 1) * 10);
+		cout << vector[i] << " ";
+	}
+	cout << endl;
+	cout << "vector size : " << vector.size() << endl;
+	cout << "capacity 값 : " << vector.capacity() << endl;
+	vector.pop_back();
+	vector.pop_back();
+	vector.reserve(10);
+	for (int i = 0; i < 15; i++)
+	{
+		vector.push_back((i + 1) * 10);
+		cout << vector[i] << " ";
+	}
+	cout << endl;
+	cout << "vector size : " << vector.size() << endl;
+	cout << "capacity 값 : " << vector.capacity() << endl;
 #pragma endregion
 
 }
