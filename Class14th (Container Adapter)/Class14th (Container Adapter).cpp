a24770fcfa2d91fe4310d7294e7c3ceabb0137b7
JeongAaron <lovejsk1004@gmail.com>
2025-02-10 19:40:45 +0900

    2025-02-10 이터레이터

---
+++ Class14th (Container Adapter)/Class14th (Container Adapter).cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int main()
+{
+#pragma region Stack
+	//	stack<int> stack;
+	//	stack.push(10);
+	//	stack.push(20);
+	//	stack.push(30);
+	//	cout << stack.size() << endl;
+	//	while (stack.empty() != 1 )
+	//	{
+	//		cout << stack.top() << endl;
+	//		stack.pop();
+	//	}
+	//	cout << stack.size() << endl;
+	//	cout << stack.empty() << endl;
+	//	std::stack<int> stack1;
+	//	stack1.push(5);
+	//	stack1.push(7);
+	//	stack1.push(9);
+	//	std::stack<int> stack2;
+	//	stack2.push(4);
+	//	stack2.push(6);
+	//	stack2.push(8);
+	//	stack2.push(10);
+	//	stack1.swap(stack2);
+	//	while (stack1.empty() != 1)
+	//	{
+	//		cout << stack1.top() << endl;
+	//		stack1.pop();
+	//	}
+#pragma endregion
+
+#pragma region Queue
+	//	std::queue<int> queue;
+	//	queue.push(10);
+	//	queue.push(20);
+	//	queue.push(30);
+	//	queue.push(40);
+	//	queue.push(50);
+	//	while (queue.empty() != 1)
+	//	{
+	//		cout << queue.front() << " ";
+	//		queue.pop();
+	//	}
+	//	cout << endl;
+#pragma endregion
+
+}
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,37 +1,8 @@
 #include <iostream>
-#include <stack>
+
 using namespace std;
 
 int main()
 {
-#pragma region Stack
-	stack<int> stack;
-	stack.push(10);
-	stack.push(20);
-	stack.push(30);
-	cout << stack.size() << endl;
-	while (stack.empty() != 1 )
-	{
-		cout << stack.top() << endl;
-		stack.pop();
-	}
-	cout << stack.size() << endl;
-	cout << stack.empty() << endl;
-	std::stack<int> stack1;
-	stack1.push(5);
-	stack1.push(7);
-	stack1.push(9);
-	std::stack<int> stack2;
-	stack2.push(4);
-	stack2.push(6);
-	stack2.push(8);
-	stack2.push(10);
-	stack1.swap(stack2);
-	while (stack1.empty() != 1)
-	{
-		cout << stack1.top() << endl;
-		stack1.pop();
-	}
-#pragma endregion
 
 }
